81512aa60e6a3f3782e55ee04264292066b5aad9
Chukwuka Chidera Princewill <cchukwuka@seamhealthgroup.com>
2021-05-20 13:20:48 +0100

    Creation of linked list, adding head, adding tail, remove head and remove tail

---
+++ linked-list.js
@@ -0,0 +1,52 @@
+
+/* Linked List */
+
+function LinkedList() {
+    this.head = null;
+    this.tail = null;
+  }
+  
+  function Node(value, next, prev) {
+    this.value = value;
+    this.next = next;
+    this.prev = prev;
+  }
+  
+  LinkedList.prototype.addToHead = function(value) {
+    var newNode = new Node(value, this.head, null);
+    if(this.head) this.head.prev = newNode;
+    else this.tail = newNode;
+    this.head = newNode; 
+  };
+  
+  LinkedList.prototype.addToTail = function(value) {
+    var newNode = new Node(value, null, this.tail);
+    if(this.tail) this.tail.next = newNode;
+    else this.head = newNode;
+    this.tail = newNode;
+  };
+  
+  LinkedList.prototype.removeHead = function() {
+    if(!this.head) return null;
+    var val = this.head.value;
+    this.head = this.head.next;
+    if(this.head) this.head.prev = null;
+    else this.tail = null;
+    return val;
+  }
+  
+  LinkedList.prototype.removeTail = function() {
+    if(!this.tail) return null;
+    var val = this.tail.value;
+    this.tail = this.tail.prev;
+    if(this.tail) this.tail.next = null;
+    else this.head = null;
+    return val;
+  }
+   
+  var ll = new LinkedList();
+  ll.addToTail(100);
+  ll.addToTail(200);
+  
+  console.log(ll.removeTail())
+  console.log(ll)
