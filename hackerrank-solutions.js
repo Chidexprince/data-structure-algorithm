7f6a731be76191da2eda80c40affe0d5dc28121f
Chukwuka Chidera Princewill <cchukwuka@seamhealthgroup.com>
2021-08-05 09:44:37 +0100

    update on fizzbuzz

--- hackerrank-solutions.js
+++ hackerrank-solutions.js
@@ -85,11 +85,11 @@ function fizzBuzz(n) {
     }
     
     for(var i = 1; i < n + 1; i++) {
-        if(i % 3 == 0 && i % 5 == 0) {
+        if(i % 3 === 0 && i % 5 === 0) {
             console.log("FizzBuzz");
-        } else if (i % 3 == 0) {
+        } else if (i % 3 == 0 && i % 5 !== 0) {
             console.log("Fizz");
-        } else if (i % 5 == 0) {
+        } else if (i % 3 !== 0 && i % 5 === 0) {
             console.log("Buzz");
         } else {
             console.log(i)
