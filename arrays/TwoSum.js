52f3f031e1b2a147c39fed511056fd32a2a45e72
Chukwuka Chidera Princewill <cchukwuka@seamhealthgroup.com>
2021-09-06 12:54:37 +0100

    Solving array questions on leetcode

--- arrays/TwoSum.js
+++ arrays/TwoSum.js
@@ -1,4 +1,15 @@
 
+/* LeetCode 1 : Given an array of integers nums and an integer target, return indices of the two numbers such that 
+they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice. 
+
+Example
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+*/
+
 
 /* Brute force approach time complexity: O(n^2), space complexity: O(1) */
 
@@ -19,6 +30,7 @@ var twoSum1 = function(nums, target) {
 
 
 /* Optimized solution with HashMap time complexity: O(n), space complexity: O(n) */
+
 var twoSum2 = function(nums, target) {
     let numsMap = {};
 
@@ -34,4 +46,55 @@ var twoSum2 = function(nums, target) {
     }
 
     return null
+};
+
+/* LeetCode: 11 Given n non-negative integers a1, a2, ..., an , where each represents a point at coordinate (i, ai). 
+n vertical lines are drawn such that the two endpoints of the line i is at (i, ai) and (i, 0). Find two lines, which, 
+together with the x-axis forms a container, such that the container contains the most water. 
+Notice that you may not slant the container.
+
+Input: height = [1,8,6,2,5,4,8,3,7]
+Output: 49
+Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. 
+In this case, the max area of water (blue section) the container can contain is 49.*/
+
+/* Brute Force Approach time complexity O(n^2), space complexity O(1)*/
+
+var maxArea1 = function(height) {  
+    let maxArea = 0;
+
+    for (let h1 = 0; h1 < height.length; h1++) {
+        for(h2 = h1 + 1; h2 < height.length; h2++) {
+            let length = Math.min(height[h1],height[h2]);
+            let width = h2 - h1;
+            let area = length * width;
+            maxArea = Math.max(maxArea, area);
+        }
+    }
+
+    return maxArea;
+};
+
+
+/* Optimized solution using pointers time complexity O(n), space complexity O(1) */
+
+var maxArea = function(height) {  
+    let p1 = 0, p2 = height.length - 1, maxArea = 0;
+    
+    while(p1 < p2) {
+        
+        let length = Math.min(height[p1],height[p2]);
+        let width = p2 - p1;
+        let area = length * width;
+        maxArea = Math.max(maxArea, area);
+        
+        if(height[p1] <= height[p2] ) {
+            p1++;
+        } else {
+            p2--
+        }
+    }
+
+
+    return maxArea;
 };
